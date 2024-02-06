d7af4358a7151aaad5a9bccaa310c8914e4463e2
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-02-06 13:21:01 +0100

    Add files via upload

---
+++ Ejercicio07.Fechas.Programacion/Ejercicio07.Fechas.Programacion/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio07.Fechas.Programacion
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("INTRODUZCA UNA FECHA: ");
+            DateTime fechaDada = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("NUMERO DE DIAS A SUMAR: ");
+            int numeroASumar = Convert.ToInt32(Console.ReadLine());
+
+
+            DateTime fechaSumada = fechaDada.AddDays(numeroASumar);
+            Console.WriteLine("DIAS SUMADOS: " + fechaSumada);
+
+
+            DateTime fechaRestada = fechaDada.AddDays(-numeroASumar);
+            Console.WriteLine("DIAS RESTADOS: " + fechaRestada);
+
+
+
+
+
+        }
+    }
+
+
+}
